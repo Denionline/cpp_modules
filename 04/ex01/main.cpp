f966e8117f19df7bcccbcda2b1e9074dcc40f6a4
Denionline <danielximenes164@gmail.com>
2026-03-29 12:15:25 +0100

    refactor: Simplify main function by removing unused code and implementing dynamic animal array

--- 04/ex01/main.cpp
+++ 04/ex01/main.cpp
@@ -3,23 +3,22 @@
 #include "Brain.hpp"
 
 int main( void ) {
-	{
-		const Animal*	j = new Dog();
-		const Animal*	i = new Cat();
+	const unsigned int	number_of_animals = 4;
+	Animal*				animals[number_of_animals];
 
-		delete j;
-		delete i;
-	}
-	{
-		class Dog dog;
+	if (number_of_animals % 2) return (1);
 
-		dog.thinkSound();
-		dog.thinkSound();
-		dog.thinkSound();
-		dog.showThoughts();
-		dog.makeSound();
-		dog.forgotSound();
-		dog.showThoughts();
+	for (unsigned int i = 0; i < number_of_animals; i++) {
+		if (i % 2) {
+			animals[i] = new Dog();
+		} else {
+			animals[i] = new Cat();
+		}
 	}
+
+	for (unsigned int i = 0; i < number_of_animals; i++) animals[i]->makeSound();
+
+	for (unsigned int i = 0; i < number_of_animals; i++) delete animals[i];
+
 	return (0);
 }
