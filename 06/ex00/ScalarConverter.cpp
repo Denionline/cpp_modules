dcf5d1cc651d279a55784a499bd543943ac0ff9e
Denionline <danielximenes164@gmail.com>
2026-04-08 12:08:35 +0100

    feat: Finished ex00

--- 06/ex00/ScalarConverter.cpp
+++ 06/ex00/ScalarConverter.cpp
@@ -1,33 +1,88 @@
 #include "ScalarConverter.hpp"
 
-void	ScalarConverter::convert( const std::string& number ) {
-	int		nInt = (int)strtol(number.c_str(), NULL, 10);
-	float	nFloat = strtof(number.c_str(), NULL);
-	double	nDouble = strtod(number.c_str(), NULL);
+static bool	numberIsValid( std::string number ) {
+	const int	size = number.length();
+	int			i;
 
-	std::cout <<  "char: " << static_cast<char>(nInt) << std::endl;
-	std::cout <<  "int: " << nInt << std::endl;
-
-	std::cout
-	<<  "float: "
-	<< std::fixed
-	<< std::setprecision(1)
-	<< nFloat
-	<< "f"
-	<< std::endl;
-
-	std::cout
-	<<  "double: "
-	<< std::fixed
-	<< std::setprecision(1)
-	<< nDouble
-	<< std::endl;
+	for (i = 0; i < size; i++) {
+		if ((number.at(i) == '-' || number.at(i) == '+') && (i + 1) != size)
+			continue;
+		if (number.at(i) == '.' && ++i)
+			break;
+		if (!std::isdigit(number.at(i)))
+			return (false);
+	}
+	if (i < size && !std::isdigit(number.at(i++)))
+		return (false);
+	return (i == size || number.at(i) == 'f');
 }
 
-/*
-./convert 42.0f
-char: '*'
-int: 42
-float: 42.0f
-double: 42.0
-*/
+static void	print( std::string prefix , std::string number , char t ) {
+	const bool	isValid = numberIsValid(number);
+
+	std::cout << prefix;
+	switch (t) {
+		case CHAR: {
+			int	nbr = strtol(number.c_str(), NULL, 10);
+			std::cout
+			<< ( isValid ?
+				( nbr > 31 && nbr < 127 ?
+					std::string(1, static_cast<char>(nbr)).c_str()
+					: "Non displayable"
+				)
+				: "impossible"
+			);
+			break;
+		}
+		case INT: {
+			if (isValid) {
+				std::cout << strtol(number.c_str(), NULL, 10);
+			} else {
+				std::cout << "impossible";
+			}
+			break;
+		}
+		case FLOAT: {
+			if (isValid) {
+				std::cout
+				<< std::fixed
+				<< std::setprecision(1)
+				<< strtof(number.c_str(), NULL)
+				<< "f";
+			} else {
+				std::cout
+				<< (number.find('-') == std::string::npos ? "+" : "-")
+				<< "inff";
+			}
+			break;
+		}
+		case DOUBLE: {
+			if (isValid) {
+				std::cout
+				<< std::fixed
+				<< std::setprecision(1)
+				<< strtod(number.c_str(), NULL);
+			} else {
+				std::cout
+				<< (number.find('-') == std::string::npos ? "+" : "-")
+				<< "inf";
+			}
+			break;
+		}
+	}
+	std::cout << std::endl;
+}
+
+void	ScalarConverter::convert( const std::string& number ) {
+	try {
+		if (number.length() > 8)
+			throw NumberTooLarge();
+		print("char: ", number, CHAR);
+		print("int: ", number, INT);
+		print("float: ", number, FLOAT);
+		print("double: ", number, DOUBLE);
+	}
+	catch( const std::exception& e ) {
+		std::cerr << e.what() << std::endl;
+	}
+}
--- 06/ex00/include/ScalarConverter.hpp
+++ 06/ex00/include/ScalarConverter.hpp
@@ -4,9 +4,24 @@
 #include <cstdlib>
 #include <string>
 #include <iomanip>
+#include <exception>
+
+enum eType {
+	CHAR = 'c',
+	INT = 'i',
+	FLOAT = 'f',
+	DOUBLE = 'd'
+};
 
 class ScalarConverter {
 	public:
 		virtual ~ScalarConverter() = 0;
 		static void	convert( const std::string& );
+
+		class NumberTooLarge: public std::exception {
+			public:
+				const char* what() const throw() {
+					return ("Number too large");
+				}
+		};
 };
