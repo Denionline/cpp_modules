e19e5816bde776c2a66d4ddd31b7da221b90c957
Denionline <danielximenes164@gmail.com>
2026-04-11 18:58:39 +0100

    feat: Add Makefile and utility functions for template operations

---
+++ 07/ex00/include/whatever.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+
+template <typename T>
+void	swap(T t1, T t2) {
+	T	t = t1;
+	t1 = t2;
+	t2 = t;
+}
+
+template <typename T>
+T	min(T t1, T t2) {
+	return ( t1 < t2 ? t1 : t2);
+}
+
+template <typename T>
+T	max(T t1, T t2) {
+	return ( t1 > t2 ? t1 : t2);
+}
