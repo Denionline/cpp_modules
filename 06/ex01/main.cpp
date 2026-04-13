232f6f23de2fb87768e06548cbfdee84acaf5b41
Denionline <danielximenes164@gmail.com>
2026-04-13 16:19:16 +0100

    fix: Improve error handling in main function for argument validation

--- 06/ex01/main.cpp
+++ 06/ex01/main.cpp
@@ -2,15 +2,16 @@
 
 int	main( int argc , char *argv[] ) {
 	(--argc, ++argv);
-	if (argc != 2) return (1);
+	if (argc != 2) {
+		std::cout << "Correct usage: ./exec <string> <number>" << std::endl;
+		return (1);
+	}
 
 	std::string	name = argv[0], number = argv[1];
 	if (name.empty() || number.empty() || number.length() > 5)
 		return (1);
-	Data		data = (Data){
-					name,
-					strtol(number.c_str(), NULL, 10)
-				};
+
+	Data		data = (Data){name, strtol(number.c_str(), NULL, 10)};
 
 	uintptr_t	s = Serializer::serialize(&data);
 
