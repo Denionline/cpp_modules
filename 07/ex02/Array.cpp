8d96d2e9e82061e1020518ed9bdf0daad84ad63c
Denionline <danielximenes164@gmail.com>
2026-04-16 11:57:36 +0100

    feat: Implement Array class with dynamic memory management and exception handling

--- 07/ex02/Array.cpp
+++
@@ -1,7 +0,0 @@
-#include "Array.hpp"
-
-Array::Array()
-{}
-
-Array::~Array()
-{}
--- 07/ex02/include/Array.hpp
+++ 07/ex02/include/Array.hpp
@@ -1,11 +1,45 @@
 #pragma once
 
+#include <exception>
 #include <iostream>
-#include "Array.tpp"
+#include <cstddef>
 
+template<typename T>
 class Array {
 		private:
+			T*					_array;
+			__uint32_t	_length;
 		public:
-			Array();
-			~Array();
+			Array(): _array(new T[0]), _length(0)
+			{}
+			Array( __uint32_t n ): _array(new T[n]), _length(n)
+			{}
+			Array( Array& copy ) {
+				*this = copy;
+			}
+			Array&	operator=( const Array& copy ) {
+				if (this != &copy) {
+					this->_array = copy._array;
+					this->_length = copy._length;
+				}
+				return (*this);
+			}
+			~Array()
+			{}
+
+			T&	operator[]( __uint32_t idx ) {
+				if (idx >= _length) throw OutOfBounds();
+				return (_array[idx]);
+			}
+
+			__uint32_t	size( void ) {
+				return (_length);
+			}
+
+			class OutOfBounds: public std::exception {
+				public:
+					const char* what() const throw() {
+						return ("Index is out of bounds!");
+					}
+			};
 };
